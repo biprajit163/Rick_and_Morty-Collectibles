9c213c1a97e5cec40eb80f4d81b43beef857ffb5
biprajit163 <biprajit163@gmail.com>
2020-12-17 13:19:44 -0500

    used fetch() to ger r&m api

--- App/app.js
+++ App/app.js
@@ -1,3 +1,17 @@
 console.log("app.js is connected!");
 
+const characterURL = "https://rickandmortyapi.com/api/character";
+
+
+fetch(characterURL, {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+        'Content-Type': 'application/json' 
+    }
+})
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => console.log("Something went wrong...", err));
+
 
