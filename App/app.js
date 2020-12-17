48b5bf09a7a9c77699ed94462bf2ca626a42f3db
biprajit163 <biprajit163@gmail.com>
2020-12-17 13:58:06 -0500

    get charcter name to appear

--- App/app.js
+++ App/app.js
@@ -1,6 +1,9 @@
 console.log("app.js is connected!");
 
-const characterURL = "https://rickandmortyapi.com/api/character";
+const cardContainer = document.querySelector(".card-container");
+
+let characterName = "rick"
+const characterURL = `https://rickandmortyapi.com/api/character/?name=${characterName}`;
 
 
 fetch(characterURL, {
@@ -11,7 +14,18 @@ fetch(characterURL, {
     }
 })
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => {
+    console.log(data.info);
+    console.log(data.results);
+
+    for(let i=0; i < data.results.length; i++) {
+        let characterCard = document.createElement("div");
+        characterCard.setAttribute("class", "character-card");
+
+        characterCard.innerText = data.results[i].name;
+        cardContainer.appendChild(characterCard);
+    }
+})
 .catch(err => console.log("Something went wrong...", err));
 
 
