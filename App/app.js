1fe9b36db2cd7db8265bcdabf88a5fb0f317031b
biprajit163 <biprajit163@gmail.com>
2020-12-17 14:52:38 -0500

    save dynamic search, can get image and name

--- App/app.js
+++ App/app.js
@@ -1,8 +1,8 @@
 console.log("app.js is connected!");
 
-const cardContainer = document.querySelector(".card-container");
+const cardContainer = document.querySelector("#card-container");
 
-let characterName = "rick"
+let characterName = "mr. mee";
 const characterURL = `https://rickandmortyapi.com/api/character/?name=${characterName}`;
 
 
@@ -17,13 +17,37 @@ fetch(characterURL, {
 .then(data => {
     console.log(data.info);
     console.log(data.results);
+    // console.log(data.results[0].image)
+    // let imageURL = `${data.results[0].image}`
+    
 
     for(let i=0; i < data.results.length; i++) {
-        let characterCard = document.createElement("div");
-        characterCard.setAttribute("class", "character-card");
+        let imageURL = data.results[i].image;
+        
 
-        characterCard.innerText = data.results[i].name;
-        cardContainer.appendChild(characterCard);
+        let card = document.createElement("div");
+        card.setAttribute("class", "card");
+        card.style.border = 'solid #fff 2px';
+        
+        let cardImage = document.createElement("img");
+        cardImage.setAttribute("class", "card-image");
+        cardImage.setAttribute("src", `${imageURL}`);
+        cardImage.style.height = '50px';
+        cardImage.style.width = '50px';
+
+        let cardName = document.createElement("p");
+        cardName.setAttribute("class", `card-name`);
+        cardName.innerText = data.results[i].name;
+
+        /*
+        let cardInfo = document.createElement("p");
+        cardInfo.setAttribute("class", "card-info");
+        */
+
+        card.appendChild(cardImage);
+        card.appendChild(cardName);
+        // card.appendChild(cardInfo);
+        cardContainer.appendChild(card);
     }
 })
 .catch(err => console.log("Something went wrong...", err));
