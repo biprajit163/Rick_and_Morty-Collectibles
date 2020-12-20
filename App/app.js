2ae2f465bbf32e97385786d37ba619e30bce9ccd
biprajit163 <biprajit163@gmail.com>
2020-12-19 23:12:07 -0500

    add info to card front and back

--- App/app.js
+++ App/app.js
@@ -5,18 +5,6 @@ const card = document.querySelector(".card");
 const searchBar = document.querySelector(".searchbar-text");
 const searchBtn = document.querySelector(".searchbar-button");
 
-/* -------------------------------------------------------------
-    Flip Effect JS
--------------------------------------------------------------- */
-// const cardboard = document.querySelector(".cardboard")
-
-// cardboard.addEventListener("click", (event) => {
-//     cardboard.classList.toggle('is-flipped');
-// });
-/* -------------------------------------------------------------
-    Flip Effect JS
--------------------------------------------------------------- */
-
 
 
 
@@ -41,7 +29,7 @@ function showCards() {
     })
     .then(res => res.json())
     .then(data => {
-        console.log(data.info);
+        // console.log(data);
 
         let page = Math.ceil(Math.random() * data.info.pages);
         let randPageURL = `https://rickandmortyapi.com/api/character/?name=${characterName}&page=${page}`; 
@@ -63,7 +51,8 @@ function showCards() {
             for(let i=0; i < newData.results.length; i++) {
 
                 let imageURL = newData.results[i].image;
-                
+                let episodes = newData.results[i].episode.length;
+
                 let card = document.createElement("div");
                 card.setAttribute("class", "card");
 
@@ -75,7 +64,7 @@ function showCards() {
 
                 let cardFaceBack = document.createElement("div");
                 cardFaceBack.setAttribute("class", "card-face card-face-back");
-                cardFaceBack.innerText = "sfhkjsdfjkds";
+                cardFaceBack.innerText = episodes;
 
 
 
@@ -85,16 +74,26 @@ function showCards() {
                 cardImage.setAttribute("src", `${imageURL}`);
         
                 let cardName = document.createElement("p");
-                cardName.setAttribute("class", `card-name`);
+                cardName.setAttribute("class", "card-name");
                 cardName.innerText = newData.results[i].name;
         
-
+                let cardInfo = document.createElement("div");
+                cardInfo.setAttribute("class", "card-info");
+                let species = document.createElement("p");
+                species.innerText = `Species: ${newData.results[i].species}`;
+                let status = document.createElement("p");
+                status.innerText = `Status: ${newData.results[i].status}`;
+                let location = document.createElement("p");
+                location.innerText = `Location: ${newData.results[i].location["name"]}`;
 
                 
+                cardInfo.appendChild(species);
+                cardInfo.appendChild(status);
+                cardInfo.appendChild(location);
+
                 cardFaceFront.appendChild(cardImage);
                 cardFaceFront.appendChild(cardName);
-                // cardFaceBack.appendChild();
-                // cardFaceBack.appendChild();
+                cardFaceFront.appendChild(cardInfo);
 
 
                 card.appendChild(cardFaceFront);
